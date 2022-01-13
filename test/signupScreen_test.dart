8a1dcc109aa192b50b2144b22b04cd8c5bb3001f
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-01-13 21:48:26 +0100

    Testing

---
+++ test/signupScreen_test.dart
@@ -0,0 +1,24 @@
+import 'package:CampiM3aya/screens/signupScreen.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('empty email returns error string', () {
+    final result = EmailFieldValidator.validate('');
+    expect(result, 'Email can\'t be empty');
+  });
+
+  test('non-empty email returns null', () {
+    final result = EmailFieldValidator.validate('email');
+    expect(result, null);
+  });
+
+  test('empty password returns error string', () {
+    final result = PasswordFieldValidator.validate('');
+    expect(result, 'Password can\'t be empty');
+  });
+
+  test('non-empty password returns null', () {
+    final result = PasswordFieldValidator.validate('password');
+    expect(result, null);
+  });
+}
